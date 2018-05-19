e485a02a93344eba962fa4f79f2dbfa0c66c6c22
Vishal <vishal.jhanjhri>
2018-05-19 18:04:44 +0530

    Add inital files

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const url = require('url');
+const dt = require('./constantFunc');
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error(err)
+    }
+    console.log('port is listening to: ', constant.port)
+})
+
+app.get('/app', (request, response) => {
+
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // });
+    response.json(getObj())
+})
+
+
+function getObj() {
+    var obj = {
+        'statusCode': 200,
+        'data': {
+            'key': 'key',
+            'value': 'value'
+        }
+    }
+    return obj
+  }
