b3567998714a46f28a9ac0216fa2e8dc145c2d57
Vishal <vishal.jhanjhri>
2018-05-21 16:30:20 +0530

    updated port

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'database': 'mongodb://dev529:vishal12@ds137110.mlab.com:37110/mongo_dev',
-    'port' : process.env.PORT || 8080,
+    'port' : process.env.PORT || 80,
     'secret': process.env.NODE_ENV === 'production' ? 'mongo_dev' : 'mongo_dev'
 }
