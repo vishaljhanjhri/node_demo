ae1fdba4a8076530c0227746f03ce9c0ca0ca794
Vishal <vishal.jhanjhri>
2018-08-26 17:55:56 +0530

    messages

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen(3001, 'localhost', function(){
+var server = app.listen(config.port, function(){
     console.log('Listening on port ' + server.address().port);
 })
